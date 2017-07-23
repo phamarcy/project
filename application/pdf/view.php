cfd7decccf9ad034713e61db7909783aac9ece4b
lactasoymix <Anawat_w@cmu.ac.th>
2017-07-23 22:40:00 +0700

    gitignore

--- application/pdf/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $file = $_GET['file'];
-
-
-$filename = "MyPDF"; 
-header("Content-type: application/pdf");
-header("Content-Disposition: inline; filename=MyPDF.pdf");
-readfile('MyPDF.pdf');
- ?>
