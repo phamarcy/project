4825032a8ad4af03628679d2762bdac805d7e5e1
benzsos <t_tofuz_bbbenz@hotmail.com>
2017-07-24 21:56:43 +0700

    ลบแถบ Search

--- page/indexteacher.php
+++ page/indexteacher.php
@@ -227,7 +227,7 @@
 			<div class="navbar-default sidebar" role="navigation">
 				<div class="sidebar-nav navbar-collapse">
 					<ul class="nav" id="side-menu">
-						<li class="sidebar-search">
+						<!--<li class="sidebar-search">
 							<div class="input-group custom-search-form">
 								<input type="text" class="form-control" placeholder="Search...">
 								<span class="input-group-btn">
@@ -236,8 +236,7 @@
 														</button>
 												</span>
 							</div>
-							<!-- /input-group -->
-						</li>
+						</li>-->
 						<li>
 							<a href="#" onclick="loadDoc('test.php')"><i class="fa fa-home fa-fw"></i> หน้าแรก</a>
 						</li>
