46820456a5568eba6892025ccc91c4387ffc41b7
adiluckyo <adiluckyo@gmail.com>
2017-08-08 15:46:20 +0700

    เปลี่ยนชื่อไฟล์ให้ใช้งานจริง เพิ่มหน้า view pdf

--- application/pdf/view.php
+++ application/pdf/view.php
@@ -1,9 +1,41 @@
 <?php
-// $file = $_GET['file'];
+if(isset($_GET['type']) && isset($_GET['info']))
+{
+    $FILE_TYPE = $_GET['type']; //draft, complete
+    $INFORMATION_TYPE = $_GET['info']; //evaluate,syllabus,special
+    $PATH_FILE = "../../files/";
+   if($INFORMATION_TYPE == 'special')
+   {
+     if(isset($_GET['id']))
+     {
+       $SPECIAL_ID = $_GET['id']; //instructor id
+       $FILE_NAME = $PATH_FILE."special_instructor/".$SPECIAL_ID.".pdf";
+     }
+     else {
+       error();
+     }
 
-
-$filename = "MyPDF"; 
-header("Content-type: application/pdf");
-header("Content-Disposition: inline; filename=MyPDF.pdf");
-readfile('MyPDF.pdf');
+   }
+   else
+   {
+     if(isset($_GET['course']))
+     {
+       $FILE_COURSE = $_GET['course'];
+       $FILE_NAME = $PATH_FILE.$FILE_TYPE."/".$FILE_COURSE."_".$INFORMATION_TYPE.".pdf";
+     }
+     else {
+       error();
+     }
+   }
+   header("Content-type: application/pdf");
+   header("Content-Disposition: inline;");
+   readfile($FILE_NAME);
+}
+else {
+  error();
+}
+function error()
+{
+  die("<center><h1>Error : Invalid data </h1></center>");
+}
  ?>
