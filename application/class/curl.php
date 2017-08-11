bfde78ce2c462b2e469acc8be2bb4745fee9628d
adiluckyo <adiluckyo@gmail.com>
2017-08-11 16:25:40 +0700

    เพิ่มการเขียน log

--- application/class/curl.php
+++ application/class/curl.php
@@ -1,6 +1,6 @@
 <?php
 require_once(__DIR__."/../config/configuration_variable.php");
-require_once("Log.php");
+require_once(__DIR__."/Log.php");
 /**
  *
  */
@@ -9,11 +9,13 @@ class CURL
   private $PROTOCOL;
   private $DIR_NAME;
   private $URL;
+  private $LOG;
   function __construct($path)
   {
     $this->PROTOCOL = isset($_SERVER['HTTPS']) ? "https:" : "http:";
   	$this->DIR_NAME = dirname(dirname((dirname($_SERVER['REQUEST_URI']))));
   	$this->URL    = $this->PROTOCOL."//".$_SERVER['HTTP_HOST'].$this->DIR_NAME."/".$path;
+    $this->LOG = new Log();
   }
   function Request($data)
   {
@@ -26,11 +28,10 @@ class CURL
   	curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
     // $output contains the output string
     $output = curl_exec($ch);
-
   	if($output=== false)
   	{
+        $this->LOG->Write('Curl error: ' . curl_error($ch));
         return false;
-  			return 'Curl error: ' . curl_error($ch);
   	}
   	else
   	{
