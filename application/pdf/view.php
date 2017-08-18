41af2b3ec2afb58f69c671c2465712058af43566
adiluckyo <adiluckyo@gmail.com>
2017-08-18 20:33:20 +0700

    เพิ่มการ view doc

--- application/pdf/view.php
+++ application/pdf/view.php
@@ -1,40 +1,78 @@
 <?php
-if(isset($_GET['type']) && isset($_GET['info']))
+require_once(__DIR__."/../class/curl.php");
+
+if( isset($_GET['info']))
 {
-    $FILE_TYPE = $_GET['type']; //draft, complete
+
     $INFORMATION_TYPE = $_GET['info']; //evaluate,syllabus,special
     $PATH_FILE = "../../files/";
    if($INFORMATION_TYPE == 'special')
    {
-     if(isset($_GET['id']))
+     header("Content-type: application/pdf");
+     if(isset($_GET['type']))
      {
-       $SPECIAL_ID = $_GET['id']; //instructor id
-       $FILE_NAME = $PATH_FILE.$FILE_TYPE."/special_instructor/".$SPECIAL_ID.".pdf";
+       $FILE_TYPE = $_GET['type']; //draft, complete
+       if(isset($_GET['id']))
+       {
+         $SPECIAL_ID = $_GET['id']; //instructor id
+         $FILE_NAME = $PATH_FILE.$FILE_TYPE."/special_instructor/".$SPECIAL_ID.".pdf";
+        header("Content-type: application/pdf");
+         header("Content-Disposition: inline;");
+         readfile($FILE_NAME);
+       }
+       else
+       {
+         error();
+       }
      }
      else
      {
        error();
      }
-
    }
    else if($INFORMATION_TYPE == 'evaluate')
    {
-     if(isset($_GET['course']))
+     header("Content-type: application/pdf");
+     if(isset($_GET['type']))
      {
-       $FILE_COURSE = $_GET['course'];
-       $FILE_NAME = $PATH_FILE.$FILE_TYPE."/".$INFORMATION_TYPE.'/'.$FILE_COURSE."_".$INFORMATION_TYPE.".pdf";
+       $FILE_TYPE = $_GET['type']; //draft, complete
+       if(isset($_GET['course']))
+       {
+         $FILE_COURSE = $_GET['course'];
+         $FILE_NAME = $PATH_FILE.$FILE_TYPE."/".$INFORMATION_TYPE.'/'.$FILE_COURSE."_".$INFORMATION_TYPE.".pdf";
+         header("Content-type: application/pdf");
+         header("Content-Disposition: inline;");
+         readfile($FILE_NAME);
+       }
+       else
+       {
+         error();
+       }
      }
      else
      {
-       error();
+        error();
      }
    }
    else if($INFORMATION_TYPE == 'syllabus')
    {
+     $curl = new CURL();
+     $server_url = $curl->GET_SERVER_URL();
      if(isset($_GET['course']))
      {
        $FILE_COURSE = $_GET['course'];
-       $FILE_NAME = $PATH_FILE."/".$INFORMATION_TYPE.'/'.$FILE_COURSE.".pdf";
+       $FILE_NAME = $PATH_FILE."/".$INFORMATION_TYPE.'/'.$FILE_COURSE.".docx";
+       if(!file_exists($FILE_NAME))
+       {
+         $FILE_NAME = $PATH_FILE."/".$INFORMATION_TYPE.'/'.$FILE_COURSE.".doc";
+         $doc_url = $server_url."/files/syllabus/".$FILE_COURSE.".doc";
+       }
+       else
+       {
+         $doc_url = $server_url."/files/syllabus/".$FILE_COURSE.".docx";
+       }
+
+       echo '<iframe src="http://docs.google.com/gview?url=".$doc_url."&embedded=true" style="width:550px; height:450px;" frameborder="0"></iframe>';
      }
      else
      {
@@ -45,9 +83,7 @@ if(isset($_GET['type']) && isset($_GET['info']))
    {
      error();
    }
-   header("Content-type: application/pdf");
-   header("Content-Disposition: inline;");
-   readfile($FILE_NAME);
+
 }
 else {
   error();
