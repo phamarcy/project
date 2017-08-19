b435e1854c5c8c582ee5135b3e783227f4242ea1
adiluckyo <adiluckyo@gmail.com>
2017-08-19 20:01:09 +0700

    เพิ่ม function ค้นหาชื่อวิชา

--- application/class/course.php
+++ application/class/course.php
@@ -17,8 +17,20 @@ class Course
   //search teacher prefix and name
   public function Get_Course_Name($course_id)
   {
+
+    $sql="SELECT `course_name_en` FROM `course_name` WHERE `course_id` ='".$course_id."'";
+
+    $result = $this->DB->Query($sql);
+    if($result)
+    {
+      $course_name = $result[0]['course_name_en'];
+      return $course_name;
+    }
+    else
+    {
+      return '-';
+    }
     //search teacher data
-    return "course name";
   }
 }
  ?>
