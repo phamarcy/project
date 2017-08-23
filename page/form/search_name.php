012afcba12e5599c7b2172180b5157e7291fefad
adiluckyo <adiluckyo@gmail.com>
2017-08-23 13:23:29 +0700

    real time search

---
+++ page/form/search_name.php
@@ -0,0 +1,23 @@
+<?php
+require_once(__DIR__.'/../../application/class/database.php');
+$db = new Database();
+$db->Change_DB('person');
+$search = $_POST['name'];
+$sql = "SELECT concat(fname,' ',lname) as name FROM `staff` WHERE concat(fname,' ',lname) LIKE '%".$search."%'";
+$result = $db->Query($sql);
+if($result)
+{
+  $data = array();
+  for($i=0;$i<count($result);$i++)
+  {
+    $name = $result[$i]['name'];
+    array_push($data,$name);
+  }
+  echo json_encode($data);
+}
+else
+{
+  $data[0] = "No data";
+  echo json_encode($data);
+}
+?>
