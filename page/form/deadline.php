ac06cd19f0c0ce55a601090b39584f0f2ce52308
adiluckyo <adiluckyo@gmail.com>
2017-09-04 10:48:27 +0700

    เชค session

--- page/form/deadline.php
+++ page/form/deadline.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if(!isset($_SESSION['level']) || !isset($_SESSION['fname']) || !isset($_SESSION['lname']) || !isset($_SESSION['id']))
+{
+    die('กรุณา Login ใหม่');
+}
 ?>
 <html>
 <header>
