eff025fc3da6c14a6ca43cef7319c7afe7acf240
adiluckyo <adiluckyo@gmail.com>
2017-09-05 15:42:37 +0700

    เชคสกุลไฟล์ก่อนอัพโหลด

--- application/document/upload_grade.php
+++ application/document/upload_grade.php
@@ -1,4 +1,3 @@
-<<<<<<< current
 <?php
 require_once(__DIR__.'/../class/manage_deadline.php');
 
@@ -22,6 +21,14 @@ function Upload($file,$course_id)
 	$path = $FILE_PATH."/grade";
 	$filename = $file['name'];
 	$ext = pathinfo($filename, PATHINFO_EXTENSION);
+  if(file_exists($path."/".$course_id."_grade_".$semester['semester']."_".$semester['year'].".xls"))
+  {
+    unlink($path."/".$course_id."_grade_".$semester['semester']."_".$semester['year'].".xls");
+  }
+  else if(file_exists($path."/".$course_id."_grade_".$semester['semester']."_".$semester['year'].".xlsx"))
+  {
+    unlink($path."/".$course_id."_grade_".$semester['semester']."_".$semester['year'].".xlsx");
+  }
 	$uploadfile = $path."/".$course_id."_grade_".$semester['semester']."_".$semester['year'].".".$ext;
 	if (!move_uploaded_file($file['tmp_name'], $uploadfile))
 	{
@@ -41,48 +48,3 @@ function Upload($file,$course_id)
 
 
  ?>
-=======
-<?php
-
-require_once(__DIR__.'/../class/manage_deadline.php');
-
-$deadline = new Deadline();
-$semester = $deadline->Get_Current_Semester();
-if(isset($_FILES['file']))
-{
-
-  $file = $_FILES['file'];
-  if(isset($_POST['course_id']))
-  {
-    $course_id = $_POST['course_id'];
-    Upload($file,$course_id);
-  }
-}
-
-function Upload($file,$course_id)
-{
-  global $semester;
-	global $FILE_PATH;
-	$path = $FILE_PATH."/grade";
-	$filename = $file['name'];
-	$ext = pathinfo($filename, PATHINFO_EXTENSION);
-	$uploadfile = $path."/".$course_id."_grade_".$semester['semester']."_".$semester['year'].".".$ext;
-	if (!move_uploaded_file($file['tmp_name'], $uploadfile))
-	{
-		$return['status'] = "error";
-		$return['msg'] = 'ไม่สามารถบันทึกไฟล์เกรดได้';
-		echo json_encode($return);
-    die();
-	}
-  else
-  {
-    $return['status'] = "success";
-		$return['msg'] = 'อัพโหลดสำเร็จ';
-    echo json_encode($return);
-  }
-}
-
-
-
- ?>
->>>>>>> before discard
