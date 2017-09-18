2bb71aecaf3fdba077ea08755fc872123c804b87
lactasoymix <Anawat_w@cmu.ac.th>
2017-09-19 00:54:25 +0700

    แก้ไม่มีรายชื่ออยู่ในระบบ

--- application/grant/approve_grant.php
+++ application/grant/approve_grant.php
@@ -13,7 +13,13 @@ if (isset($_POST["type"]) && isset($_POST["teacher"])) {
     $date            = $_POST["date"];
   }
 
-
+  $teacher_id    = $person->Get_Teacher_Id($teacher);
+  if (!$teacher_id) {
+    $DATA          = array('status'=>'error','msg' => "ไม่มีรายชื่อนี้ในระบบ");
+    echo json_encode($DATA);
+    return $DATA;
+  }
+    
   if ($type=="add") {
 
     $checkperson     = "SELECT count(*) as item FROM grant_approve WHERE status = '1'";
@@ -27,12 +33,12 @@ if (isset($_POST["type"]) && isset($_POST["teacher"])) {
     else {
 
       $datesplit     = explode('-',$date);
-
       $datebase      = date('Y-m-d',strtotime($datesplit[0]));
       $dateplus      = date('Y-m-d',strtotime($datesplit[1]));
       $teacher_id    = $person->Get_Teacher_Id($teacher);
       $sqlcheck      = "SELECT count(*) as item FROM grant_approve WHERE user_id = ".$teacher_id."";
       $check_teacher = $DB->Query($sqlcheck);
+      
       if (($check_teacher[0]['item'])==0) {
         $sql         = "INSERT INTO grant_approve(user_id, status, date_start, date_end) VALUES ('".$teacher_id."','1','".$datebase."','".$dateplus."') ";
         $result      = $DB->Insert_Update_Delete($sql);
