52b60ef9f99d07edb6a075239263a1fc54ff65d8
benzsos <t_tofuz_bbbenz@hotmail.com>
2017-09-26 14:55:01 +0700

    เพิ่ม/ลบ/แก้ไขกระบวนวิชา

--- page/form/addsubject.php
+++ page/form/addsubject.php
@@ -65,9 +65,118 @@
 
 <script id="contentScript">
 
+function delsubj() {
+  swal({
+    title: '',
+    text: 'ท่านต้องการลบกระบวนวิชานี้ในระบบหรือไม่?',
+    type: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Ok',
+    cancelButtonText: 'Cancel'
+  }).then(function () {
 
-function submitfunc(casesubmit) {
 
+  }, function (dismiss) {
+  // dismiss can be 'cancel', 'overlay',
+  // 'close', and 'timer'
+  if (dismiss === 'cancel') {
+
+  }
+})
+}
+
+function checksubject() {
+
+  if($('#COURSE_ID').val()=="" ||$('#COURSE_ID').val()==null || $('#COURSE_ID').val().length != 6 )
+  {
+    swal(
+       '',
+       'กรุณากรอกรหัสกระบวนวิชาให้ถูกต้อง',
+       'error'
+     )
+  }
+  else {
+  var file_data = new FormData;
+  var course_id = document.getElementById('COURSE_ID').value;
+  JSON.stringify(course_id);
+  file_data.append("course_id",course_id);
+  var URL = '../../application/document/search_document.php';
+  $.ajax({
+                url: URL,
+                dataType: 'text',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: file_data,
+                type: 'post',
+                success: function (result) {
+                      try {
+                        var temp = $.parseJSON(result);
+                      } catch (e) {
+                           console.log('Error#542-decode error');
+                      }
+
+                    if(temp['info']==false && temp[0]==null){
+
+                        swal({
+                          title: '',
+                          text: 'กระบวนวิชาที่ค้นหาไม่พบในระบบ <br> ท่านต้องการที่จะเพิ่มกระบวนวิชาใหม่หรือไม่?',
+                          type: 'question',
+                          showCancelButton: true,
+                          confirmButtonColor: '#3085d6',
+                          cancelButtonColor: '#d33',
+                          confirmButtonText: 'Ok',
+                          cancelButtonText: 'Cancel'
+                        }).then(function () {
+                          $('#typesubmit').val("add");
+                          $('#deletebtn').prop('disabled', true);
+                          document.getElementById('NAME_ENG_COURSE').value = '';
+                          document.getElementById('NAME_TH_COURSE').value = '';
+                          document.getElementById('TOTAL').value = '';
+
+                        }, function (dismiss) {
+                        // dismiss can be 'cancel', 'overlay',
+                        // 'close', and 'timer'
+                        if (dismiss === 'cancel') {
+
+                        }
+                      })
+                     }
+                     else if(temp['info']!=false && temp[0]!=null){
+                        swal(
+                           'สำเร็จ',
+                           'ดึงข้อมูลสำเร็จ <br>สามารถแก้ไขรายละเอียดได้ดังแบบฟอร์มข้างล่าง',
+                           'success'
+                         )
+                        $('#typesubmit').val("edit");
+                        $('#deletebtn').prop('disabled', false);
+                        document.getElementById('COURSE_ID').value = temp['info']['course_id'];
+                        document.getElementById('NAME_ENG_COURSE').value = temp['info']['course_name_en'];
+                        document.getElementById('NAME_TH_COURSE').value = temp['info']['course_name_th'];
+                        document.getElementById('TOTAL').value = temp['info']['credit']+"("+temp['info']['hr_lec']+"-"+temp['info']['hr_lab']+"-"+temp['info']['hr_self']+")";
+                      }
+
+                },
+                failure: function (result) {
+                     alert(result);
+                },
+                error: function (xhr, status, p3, p4) {
+                     var err = "Error " + " " + status + " " + p3 + " " + p4;
+                     if (xhr.responseText && xhr.responseText[0] == "{")
+                          err = JSON.parse(xhr.responseText).Message;
+                     console.log(err);
+                }
+     });
+
+    }
+}
+
+function submitfunc() {
+
+  //typesubmit
+  var typesubmit = document.getElementById('typesubmit').value;
   //CREDIT
   var splitor = document.getElementById("TOTAL").value;
   var total = splitor.charAt(0);
@@ -83,66 +192,18 @@ function submitfunc(casesubmit) {
       'LEC' : lec,
       'LAB' : lab,
       'SELF' : self
-    },
-    /*'TYPE_TEACHING' : document.querySelector("input[name='TYPE_TEACHING']:checked").value,
-    'TYPE_TEACHING_NAME' : document.getElementById('TYPE_TEACHING_NAME').value,
-    'EVALUATE' : document.querySelector("input[name='EVALUATE_TYPE']:checked").value,
-    'CALCULATE' : {
-      'TYPE' : document.querySelector("input[name='CALCULATE']:checked").value,
-      'EXPLAINATION' : document.getElementById("EXPLAINATION").value,
-      'A' : {
-        'MIN' : document.getElementById("CALCULATE_A_MIN").value
-      },
-      'B+' : {
-        'MIN' : document.getElementById("CALCULATE_Bp_MIN").value,
-        'MAX' : document.getElementById("CALCULATE_Bp_MAX").value
-      },
-      'B' : {
-        'MIN' : document.getElementById("CALCULATE_B_MIN").value,
-        'MAX' : document.getElementById("CALCULATE_B_MAX").value
-      },
-      'C+' : {
-        'MIN' : document.getElementById("CALCULATE_Cp_MIN").value,
-        'MAX' : document.getElementById("CALCULATE_Cp_MAX").value
-      },
-      'C' : {
-        'MIN' : document.getElementById("CALCULATE_C_MIN").value,
-        'MAX' : document.getElementById("CALCULATE_C_MAX").value
-      },
-      'D+' : {
-        'MIN' : document.getElementById("CALCULATE_Dp_MIN").value,
-        'MAX' : document.getElementById("CALCULATE_Dp_MAX").value
-      },
-      'D' : {
-        'MIN' : document.getElementById("CALCULATE_D_MIN").value,
-        'MAX' : document.getElementById("CALCULATE_D_MAX").value
-      },
-      'F' : {
-        'MAX' : document.getElementById("CALCULATE_F_MAX").value
-      },
-      'S' : {
-        'MIN' : document.getElementById("CALCULATE_S_MIN").value
-      },
-      'U' : {
-        'MAX' : document.getElementById("CALCULATE_U_MAX").value
-      },
-      'OTHERGRADE' : document.getElementById("CALOTHER").value
-    },
-    'ABSENT' : document.querySelector("input[name='ABSENT']:checked").value,*/
-    'SUBMIT_TYPE' : casesubmit
+    }
   };
 
-  //console.log(JSON.stringify(data));
-  if(casesubmit=='1')
-  {
-    senddata(JSON.stringify(data));
-  }
+  //console.log(JSON.stringify(data),JSON.stringify(typesubmit));
+  senddata(JSON.stringify(data),JSON.stringify(typesubmit));
 
 }
-function senddata(data)
+function senddata(data,typesubmit)
 {
    var file_data = new FormData;
    file_data.append("DATA",data);
+   file_data.append("TYPE",typesubmit);
    var URL = '../../application/subject/add_course.php';
    $.ajax({
                  url: URL,
@@ -352,6 +413,7 @@ $(document).ready(function(){
 
       });
 
+
 });
 
 function other_type() {
@@ -435,13 +497,14 @@ function confreset(casereset) {
   <div id="wrapper" style="padding-left: 30px; padding-right: 30px;">
 <div class="row">
   <center>
-    <h3 class="page-header">เพิ่มกระบวนวิชา</h3>
+    <h3 class="page-header"><b>เพิ่มกระบวนวิชา</b></h3>
 
 
-  </center>
 </div>
 
-<div class="panel panel-default">
+</center>
+
+<div class="panel panel-default" id="panelbody">
 <form data-toggle="validator" role="form" name="form1" id="form1" method="post" onsubmit="checkreq('1')">
     <ol>
       <br>
@@ -449,14 +512,9 @@ function confreset(casereset) {
         <div class="form-inline">
           <div class="form-group">
           <b>รหัสกระบวนวิชา</b> &nbsp;<input style="width: 100px;" type="text" class="form-control numonly" name="COURSE_ID" id="COURSE_ID"  placeholder="e.g. 204111"  maxlength="6" required pattern=".{6,6}" >
+          &nbsp;<button type="button" class="btn btn-outline btn-primary" onclick="checksubject();">ค้นหา</button>
+
           </div>
-          <!--<div class="form-group">
-            &nbsp;จำนวนตอนที่ (ทั้งหมด) &nbsp;<input style="width: 70px;"type="text" class="form-control numonly" name="SECTION" id="SECTION" size="2" maxlength="2" required pattern=".{1,2}" >
-          </div>
-          <div class="form-group"><div class="radio">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="NORORSPE" id="NORORSPE1" value="NORMAL" checked>&nbsp;<b>ภาคปกติ</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input type="radio" name="NORORSPE" id="NORORSPE2" value="SPECIAL">&nbsp;<b>ภาคพิเศษ</b>
-          </div></div>-->
           <br>
           <div class="form-group">
           ชื่อกระบวนวิชาภาษาไทย &nbsp;<input style="width: 500px;" type="text" class="form-control" name="NAME_TH_COURSE" id="NAME_TH_COURSE"   maxlength="50" required placeholder="e.g. การเรียนรู้นอกห้องเรียนเพื่อเสริมสร้างประสบการณ์ชีวิต">
@@ -469,146 +527,17 @@ function confreset(casereset) {
             <div class=" form-group">&nbsp;&nbsp;&nbsp;&nbsp;จำนวนหน่วยกิตทั้งหมด &nbsp;<input type="text" class="form-control" name="TOTAL" id="TOTAL" size="7" maxlength="10" required pattern=".{8,10}" placeholder="e.g. 3(3-0-6)" >&nbsp; หน่วยกิต
             </div>
           </div>
-
+          <input type="hidden" id="typesubmit" name="typesubmit" value="">
         </div>
       </li>
       <br>
-      <!--<li style="font-size: 14px">
-        <div class="form-inline">
-          <b>ลักษณะการเรียนการสอน&nbsp;&nbsp;</b><br>
-          <div class="form-group"><div class="radio">
-            <input type="radio" name="TYPE_TEACHING" id="TYPE_TEACHING1" value="LEC" required> บรรยาย &nbsp;<br>
-            <input type="radio" name="TYPE_TEACHING" id="TYPE_TEACHING2" value="LECLAB"> บรรยายและปฏิบัติการ&nbsp;<br>
-            <input type="radio" name="TYPE_TEACHING" id="TYPE_TEACHING3" value="SPE"> โครงงานทางเภสัชกรรม&nbsp;<br>
-            <input type="radio" name="TYPE_TEACHING" id="TYPE_TEACHING4" value="TRA"> ฝึกงาน &nbsp;<br>
-            <input type="radio" name="TYPE_TEACHING" id="TYPE_TEACHING5" value="SEM"> สัมมนา &nbsp;<br>
-            <input type="radio" name="TYPE_TEACHING" id="TYPE_TEACHING6" value="LAB"> ปฏิบัติการ &nbsp;<br>
-            <input type="radio" name="TYPE_TEACHING" id="TYPE_TEACHING7" value="OTH" onchange="other_type()"> อื่นๆ &nbsp;
-          </div>
-        </div><br>
-            <input type="text" class="form-control" name="TYPE_TEACHING_NAME" id="TYPE_TEACHING_NAME" placeholder="โปรดระบุ">
 
-        </div>
-      </li>
-
-      <br>
-          <li style="font-size: 14px;">
-            <b>วิธีการตัดเกรด</b>
-            <br>
-            <div class="form-inline">
-              <div class="form-group form-inline"><div class="radio">
-              <input type="radio" name="CALCULATE" id="CALCULATE_TYPE1" value="GROUP" required> อิงกลุ่ม &nbsp;
-              <input type="text" class="form-control" name="EXPLAINATION" id="EXPLAINATION" placeholder="โปรดระบุ">
-              <br><input type="radio" name="CALCULATE" id="CALCULATE_TYPE2" value="CRITERIA" checked> อิงเกณฑ์ &nbsp;&nbsp;ได้กำหนดเกณฑ์ดังต่อไปนี้
-            </div></div>
-              <br><b class="opacity01">การประเมินผล</b><br>
-              <div class="form-group opacity01"><div class="radio">
-                <input type="radio" name="EVALUATE_TYPE" id="EVALUATE1" value="AF" required> ให้ลำดับขั้น A, B+ ,B, C+, C, D+, D, F <br>
-                <input type="radio" name="EVALUATE_TYPE" id="EVALUATE2" value="SU"> ให้อักษร S หรือ U (ได้รับการอนุมัติจากมหาวิทยาลัยแล้ว)
-              </div></div>
-
-          </div>
-            <br>
-            <div class="row">
-            <div class="col-md-10">
-            <table class="table table-hover" style="font-size: 14px; ">
-              <tr align="center">
-                <th>เกรด</th>
-                <th>คะแนนต่ำสุด</th>
-                <th></th>
-                <th>คะแนนสูงสุด</th>
-                <th></th>
-                <th>เกรด</th>
-                <th>คะแนนต่ำสุด</th>
-                <th></th>
-                <th>คะแนนสูงสุด</th>
-                <th></th>
-              </tr>
-              <tr align="center">
-                <td>A</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_A_MIN" id="CALCULATE_A_MIN" maxlength="5" value="80.0"></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly" name="CALCULATE_A_MAX" id="CALCULATE_A_MAX" placeholder="100" disabled></td>
-                <td></td>
-                <td>D+</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_Dp_MIN" id="CALCULATE_Dp_MIN" maxlength="5" value="55.0"></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_Dp_MAX" id="CALCULATE_Dp_MAX" maxlength="5" value="59.9"></td>
-                <td></td>
-              </tr>
-              <tr align="center">
-                <td>B+</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_Bp_MIN" id="CALCULATE_Bp_MIN" maxlength="5" value="75.0"></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_Bp_MAX" id="CALCULATE_Bp_MAX" maxlength="5" value="79.9"></td>
-                <td></td>
-                <td>D</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_D_MIN" id="CALCULATE_D_MIN" maxlength="5" value="50.0"></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_D_MAX" id="CALCULATE_D_MAX" maxlength="5" value="54.9"></td>
-                <td></td>
-              </tr>
-              <tr align="center">
-                <td>B</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_B_MIN" id="CALCULATE_B_MIN" maxlength="5" value="70.0"></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_B_MAX" id="CALCULATE_B_MAX" maxlength="5" value="74.9"></td>
-                <td></td>
-                <td>F</td>
-                <td><input type="text" class="form-control numonly" name="CALCULATE_F_MIN" id="CALCULATE_F_MIN" placeholder="0" disabled></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_F_MAX" id="CALCULATE_F_MAX" maxlength="5" value="49.9"></td>
-                <td></td>
-              </tr>
-              <tr align="center">
-                <td>C+</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_Cp_MIN" id="CALCULATE_Cp_MIN" maxlength="5" value="65.0"></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_Cp_MAX" id="CALCULATE_Cp_MAX" maxlength="5" value="69.9"></td>
-                <td></td>
-                <td>S</td>
-                <td><input type="text" class="form-control numonly stou" name="CALCULATE_S_MIN" id="CALCULATE_S_MIN" maxlength="5" value=""></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly" name="CALCULATE_S_MAX" id="CALCULATE_S_MIN" placeholder="100" disabled></td>
-                <td></td>
-              </tr>
-              <tr align="center">
-                <td>C</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_C_MIN" id="CALCULATE_C_MIN" maxlength="5" value="60.0"></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly atof" name="CALCULATE_C_MAX" id="CALCULATE_C_MAX" maxlength="5" value="64.9"></td>
-                <td></td>
-                <td>U</td>
-                <td><input type="text" class="form-control numonly" name="CALCULATE_U_MAX" id="CALCULATE_U_MIN" placeholder="0" disabled></td>
-                <td>ถึง</td>
-                <td><input type="text" class="form-control numonly stou" name="CALCULATE_U_MAX" id="CALCULATE_U_MAX" maxlength="5" value=""></td>
-                <td></td>
-              </tr>
-            </table>
-          </div>
-        </div>
-          <div class="form-inline">
-            <b>อื่นๆ</b><br>
-            <textarea class="form-control" name="CALOTHER" id="CALOTHER" rows="4" cols="125" ></textarea>
-          </div>
-
-
-          </li>
-
-          <br>
-          <li style="font-size: 14px;">
-            <b>นักศึกษาที่ขาดสอบในการวัดผลครั้งสุดท้าย</b> &nbsp;&nbsp;โดยไม่ได้รับอนุญาตให้เลื่อนการสอบตามข้อบังคับฯ ของมหาวิทยาลัยเชียงใหม่ ว่าด้วยการศึกษาชั้นปริญญาตรี อาจารย์ผู้สอนจะประเมินดังนี้
-            <br>
-            <div class="form-inline"><div class="form-group"><div class="radio">
-            <input type="radio" name="ABSENT" id="ABSENT1" value="F" required>&nbsp;ให้ลำดับขั้น F &nbsp;&nbsp; <br>
-            <input type="radio" name="ABSENT" id="ABSENT2" value="U" >&nbsp;ให้อักษร U &nbsp;&nbsp;<br>
-            <input type="radio" name="ABSENT" id="ABSENT3" value="CAL" >&nbsp;นำคะแนนทั้งหมดที่นักศึกษาได้รับก่อนการสอบไล่มาประเมิน &nbsp;&nbsp;<br>
-          </div></div></div>
-        </li>-->
     </ol>
     <div align="center">
       <input type="submit" style="font-size: 18px;" class="btn btn-outline btn-success" name="submitbtn" id="submitbtn" value="ยืนยันเพื่อส่งข้อมูล" > &nbsp;
       <input type="button" style="font-size: 18px;" class="btn btn-outline btn-danger" name="resetbtn" id="resetbtn" onclick="confreset();" value="รีเซ็ตข้อมูล">
+      &nbsp;&nbsp;<input type="button" style="font-size: 18px;" class="btn btn-outline btn-danger" name="deletebtn" id="deletebtn" onclick="delsubj();" value="ลบข้อมูลกระบวนวิชา" disabled>
+
     </div>
 </form>
 </div>
