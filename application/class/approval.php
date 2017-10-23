b2669b3586338a2ae397ce724e2fb3acd0ce81ec
adiluck chooprateep <adiluckyo@gmail.com>
2017-10-23 22:03:57 +0700

    Revert "แก้บัคมอบอำาจ"
    This reverts commit 8ece242f10658c97d22e1408010c0613d61c2840.

--- application/class/approval.php
+++ application/class/approval.php
@@ -640,6 +640,14 @@ class approval
        WHERE sa.`assessor_group_num` = ga.`group_num` AND ga.`teacher_id` = '".$teacher_id."'
        AND ac.`status` >= '1' AND ac.`course_id` = sa.`course_id` AND ac.`semester_id` =".$this->SEMESTER_ID;
     }
+<<<<<<< HEAD
+=======
+    else
+    {
+      $sql = "SELECT DISTINCT `course_id` FROM `approval_course`
+      WHERE `status` = '5' AND `semester_id` =".$this->SEMESTER_ID;
+    }
+>>>>>>> parent of 8ece242... แก้บัคมอบอำาจ
 
     $result = $this->DB->Query($sql);
     if($result)
@@ -709,6 +717,14 @@ class approval
        WHERE sa.`assessor_group_num` = ga.`group_num` AND ga.`teacher_id` = '".$teacher_id."'
        AND ac.`status` >= '1' AND ac.`course_id` = sa.`course_id` AND ac.`semester_id` = ".$this->SEMESTER_ID;
     }
+<<<<<<< HEAD
+=======
+    else
+    {
+      $sql = "SELECT DISTINCT `course_id` FROM `approval_special`
+      WHERE `status` = '5' AND `semester_id` =".$this->SEMESTER_ID;
+    }
+>>>>>>> parent of 8ece242... แก้บัคมอบอำาจ
     $result = $this->DB->Query($sql);
     if($result)
     {
