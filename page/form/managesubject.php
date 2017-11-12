56f5974ff3c696d24759756b0d6177eda0d8e69a
lactasoymix <Anawat_w@cmu.ac.th>
2017-11-12 14:56:16 +0700

    นิดหน่อย

--- page/form/managesubject.php
+++ page/form/managesubject.php
@@ -1113,7 +1113,7 @@ else {
       function searchname(no, type) {
         var name_s = $("#TEACHERLEC_" + no).val();
         $("#dtl" + no).html('');
-        if (undefined !== name_s && name_s.length > 0) {
+        if (name_s.length > 0) {
           $.post("search_name.php", {
               name: name_s
             }, function (data) {
