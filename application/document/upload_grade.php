7692c92550d428cbe692c589bc73efd1a13beb4f
adiluck chooprateep <adiluckyo@gmail.com>
2017-11-19 17:36:02 +0700

    อัพโหลดเกรด + pdf

--- application/document/upload_grade.php
+++ application/document/upload_grade.php
@@ -3,26 +3,27 @@ require_once(__DIR__.'/../class/manage_deadline.php');
 require_once(__DIR__.'/../class/database.php');
 $db = new Database();
 $deadline = new Deadline();
-$semester = $deadline->Get_Current_Semester();
 if(isset($_FILES['file']))
 {
 
   $file = $_FILES['file'];
-  if(isset($_POST['course_id']))
+  if(isset($_POST['course_id']) && isset($_POST['semester']))
   {
+    $semester_id = $_POST['semester'];
     $course_id = $_POST['course_id'];
-    Upload($file,$course_id);
+    Upload($file,$course_id,$semester_id);
   }
 }
 
-function Upload($file,$course_id)
+function Upload($file,$course_id,$semester_id)
 {
-  global $semester,$db,$FILE_PATH;
+  global $db,$FILE_PATH,$deadline;
 	$path = $FILE_PATH."/grade";
   if(!file_exists($path))
   {
     mkdir($path);
   }
+  $semester = $deadline->Search_Semester_Term($semester_id);
 	$filename = $file['name'];
 	$ext = pathinfo($filename, PATHINFO_EXTENSION);
   if(file_exists($path."/".$course_id."_grade_".$semester['semester']."_".$semester['year'].".xls"))
@@ -35,7 +36,7 @@ function Upload($file,$course_id)
   }
   $filename = $course_id."_grade_".$semester['semester']."_".$semester['year'].".".$ext;
 	$uploadfile = $path."/".$filename;
-  $sql = "INSERT INTO `couse_grade`(`course_id`, `semester_id`, `file_name`) VALUES ('".$course_id."',".$semester['id'].",'".$filename."') ";
+  $sql = "INSERT INTO `couse_grade`(`course_id`, `semester_id`, `file_name`) VALUES ('".$course_id."',".$semester_id.",'".$filename."') ";
   $sql .= "ON DUPLICATE KEY UPDATE `file_name` = '".$filename."'";
   $result = $db->Insert_Update_Delete($sql);
 	if (!move_uploaded_file($file['tmp_name'], $uploadfile) || !$result)
