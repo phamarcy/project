1259395bc6f38be53dcbadc11509ce6a2ee47496
benzsos <t_tofuz_bbbenz@hotmail.com>
2017-11-26 21:28:24 +0700

    แก้ที่โย่บอก

--- page/form/spclteacher.php
+++ page/form/spclteacher.php
@@ -433,7 +433,7 @@ li {
    document.getElementById('mobile').value = temp['phone_mobile'];
    document.getElementById('email').value = temp['email'];
    //var choice1 = temp['TEACHERDATA']['HISTORY'];
-   $('input[name="topic"][value="already"]').prop('checked', true);
+   $('input[name="topic"][value="yet"]').prop('checked', true);
    if(document.querySelector("input[name='topic']:checked").value=="already")
      {
        $('#cvlist').hide();
@@ -808,7 +808,7 @@ li {
      $('#course').val(course_id);
      $('#fname').val(fname);
      $('#lname').val(lname);
-     $('input[name="topic"][value="yet"]').prop('checked', true);
+     $("#topic2").prop("checked", true);
      JSON.stringify(fname);
      JSON.stringify(lname);
      JSON.stringify(type);
