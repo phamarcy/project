952c9b67ed8984e7a0be459df6e6e38b37ee55d5
adiluck chooprateep <adiluckyo@gmail.com>
2018-02-09 23:07:35 +0700

    แก้คำ

--- page/form/summary.php
+++ page/form/summary.php
@@ -313,11 +313,11 @@ $COURSE_OBJ = new Course();
                                         $absent = '';
                                         if($data[$i]['absent'] == 'F')
                                         {
-                                            $absent = 'F';
+                                            $absent = 'ให้ลำดับขั้น F';
                                         }
                                         else if($data[$i]['absent'] == 'U')
                                         {
-                                            $absent = 'U';
+                                            $absent = 'ให้อักษร U';
                                         }
                                         else if($data[$i]['absent'] == 'CAL')
                                         {
@@ -474,11 +474,11 @@ $COURSE_OBJ = new Course();
                                         $absent = '';
                                         if($data[$i]['absent'] == 'F')
                                         {
-                                            $absent = 'F';
+                                            $absent = 'ให้ลำดับขั้น F';
                                         }
                                         else if($data[$i]['absent'] == 'U')
                                         {
-                                            $absent = 'U';
+                                            $absent = 'ให้อักษร U';
                                         }
                                         else if($data[$i]['absent'] == 'CAL')
                                         {
